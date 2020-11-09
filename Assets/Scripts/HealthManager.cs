eb4aa1e74f6da0a83ed3a30e17cf50b7045ce010
Sada R. Nichols-Worley <sada.r.nichols-worley.22@dartmouth.edu>
2020-11-08 20:12:36 -0500

    local reactions working; starting attempt at integrating into network

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    private int redleft;
+    private int blueleft;
+    public PlayerStats red1;
+    public PlayerStats red2;
+    public PlayerStats red3;
+    public PlayerStats blue1;
+    public PlayerStats blue2;
+    public PlayerStats blue3;
+    private ArrayList players;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        redleft = 3;
+        blueleft = 3;
+        players = new ArrayList(new PlayerStats[] { red1, red2, red3, blue1, blue2, blue3 });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string winner = didWin();
+        if (winner != null)
+        {
+            // end the game, show the winner
+        }
+    }
+
+    private string didWin()
+    {
+        if (redleft == 0)
+        {
+            return "red";
+        }
+
+        else if (blueleft == 0) {
+            return "blue";
+        }
+
+        return null;
+    }
+
+    public void wasHit(OVRPlayerController bender)
+    {
+        foreach (PlayerStats pstats in players)
+        {
+            if (!pstats || pstats == null)
+            {
+                continue;
+            }
+
+            if (pstats.player == bender)
+            {
+                pstats.health -= 1;
+                if (pstats.health == 0)
+                {
+                    if (pstats.team == 0)
+                    {
+                        redleft -= 1;
+                    }
+
+                    else
+                    {
+                        blueleft -= 1;
+                    }
+                }
+
+                handleHit(pstats);
+            }
+        }
+    }
+
+    private void handleHit(PlayerStats bender)
+    {
+        // get controller and send haptics
+
+
+
+        // depending on health, bloody screen
+        if (bender.health < 6)
+        {
+
+        }
+
+        if (bender.health < 3)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : ScriptableObject
+{
+    public int health = 10;
+    // 0 for red, 1 for blue
+    public int team;
+    private int hits;
+    private int knockouts;
+    public OVRPlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
