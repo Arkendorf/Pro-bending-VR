023a066c6f31ae6a0a7bc0641c60685279f8e753
Arkendorf <amon@ferri.us>
2020-11-09 21:19:07 -0500

    networked grabbing

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class AvatarController : MonoBehaviour
 {
     public Transform controller;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+
         transform.position = controller.position + Vector3.down;
         transform.rotation = controller.rotation;
     }
